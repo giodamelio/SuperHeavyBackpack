58b4a1928dc1deba6de934688351f2b64ef724c5
Gio d'Amelio <giodamelio@gmail.com>
2015-04-16 14:34:44 -0700

    Create basic empty mod

---
+++ src/main/java/com/giodamelio/SuperHeavyBackpack/SuperHeavyBackpack.java
@@ -0,0 +1,27 @@
+package com.giodamelio.SuperHeavyBackpack;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = SuperHeavyBackpack.MODID, version = SuperHeavyBackpack.VERSION)
+public class SuperHeavyBackpack
+{
+    public static final String MODID = "superheavybackpack";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
